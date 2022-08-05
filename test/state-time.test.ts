b018e7794d58050e9d5d837aad79a8c2bda6e486
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-08-05 15:00:59 +0100

    Restore test, which was slightly different but not clear

--- test/state-time.test.ts
+++ test/state-time.test.ts
@@ -27,6 +27,17 @@ describe("dust state at multiple time points", () => {
         const state = filledDustStateTime(nState, nParticles, nTime);
         const t0 = state.viewTime(0);
         const t5 = state.viewTime(5);
+        expect(state.getParticle(0, 0)).toEqual(t0.getParticle(0));
+        expect(state.getParticle(3, 0)).toEqual(t0.getParticle(3));
+        expect(state.getParticle(0, 5)).toEqual(t5.getParticle(0));
+        expect(state.getParticle(3, 5)).toEqual(t5.getParticle(3));
+    });
+
+    it("can extract a state at a specific time", () => {
+        // Similar to above, but using getState, not getParticle
+        const state = filledDustStateTime(nState, nParticles, nTime);
+        const t0 = state.viewTime(0);
+        const t5 = state.viewTime(5);
         expect(state.getState(0, 0)).toEqual(t0.getState(0));
         expect(state.getState(3, 0)).toEqual(t0.getState(3));
         expect(state.getState(0, 5)).toEqual(t5.getState(0));
