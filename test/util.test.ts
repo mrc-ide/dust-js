64d068e82ee56383ae4e74ab12e2545f2242110a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2022-11-01 18:00:41 +0000

    Implement and use binary search

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,4 +1,4 @@
-import { combinations, seq } from "../src/util";
+import { combinations, findClosest, search, seq } from "../src/util";
 
 describe("seq", () => {
     it("generates an inclusive sequence", () => {
@@ -18,4 +18,59 @@ describe("combinations", () => {
     expect(combinations([2, 3])).toEqual([
         [1, 1], [2, 1], [1, 2], [2, 2], [1, 3], [2, 3]
     ]);
+});
+
+describe("search", () => {
+    it("can search simple list", () => {
+        const x = seq(1, 5);
+        expect(search(x, (el) => el > -5)).toBe(0);
+        expect(search(x, (el) => el >= 2)).toBe(1);
+        expect(search(x, (el) => el > 2)).toBe(2);
+        expect(search(x, (el) => el > 2.5)).toBe(2);
+        expect(search(x, (el) => el >= 5)).toBe(4);
+        expect(search(x, (el) => el > 5)).toBe(5);
+    });
+
+    it("searches efficiently", () => {
+        let n = 0;
+        const target = (x: number) => {
+            n++;
+            return x > 1234;
+        }
+        const x = seq(1, 10000);
+        expect(search(x, target)).toBe(1234);
+        expect(n).toBe(16);
+        expect(target(x[1233])).toBe(false);
+        expect(target(x[1234])).toBe(true);
+    });
+});
+
+describe("findClosest", () => {
+    it("finds exact matches", () => {
+        const arr = seq(4, 50);
+        // one in detail:
+        const i = findClosest(10, arr);
+        expect(i).toBe(6);
+        expect(arr[i]).toBe(10);
+        // just do the lot:
+        arr.forEach((x) => expect(arr[findClosest(x, arr)]).toBe(x));
+    });
+
+    it("finds close matches", () => {
+        const arr = seq(4, 50);
+        // one in detail:
+        const i = findClosest(10.001, arr);
+        expect(i).toBe(6);
+        expect(arr[i]).toBe(10);
+        // just do the lot:
+        arr.forEach((x) => expect(arr[findClosest(x + 0.001, arr)]).toBe(x));
+    });
+
+    it("finds distant matches", () => {
+        const arr = seq(4, 50);
+        const i = findClosest(9.8, arr);
+        expect(i).toBe(6);
+        expect(arr[i]).toBe(10);
+        arr.forEach((x) => expect(arr[findClosest(x - 0.2, arr)]).toBe(x));
+    });
 });
